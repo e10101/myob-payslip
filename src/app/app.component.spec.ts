dfcfcbc71047593212c2f02f82080ef63b295efd
e10101 <baicaibang@gmail.com>
2018-09-17 23:42:22 +1200

    #6 Fix the unit and e2e tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to myob-payslip!');
+    expect(compiled.querySelector('h1').textContent).toContain('Calculator');
   }));
 });
